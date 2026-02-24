7cacca564e386a8a825ce923fda3cb21cb52b87e
J-Mro <j.k.mroueh@gmail.com>
2026-02-24 16:35:10 +0000

    add fetchArticlesById util function

--- src/utils/fetchArticleData.js
+++ src/utils/fetchArticleData.js
@@ -1,10 +1,23 @@
-// import axios from "axios";
 export function fetchArticleData() {
   return fetch("https://nc-back-end.onrender.com/api/articles").then((res) => {
     if (!res.ok) {
       return Promise.reject({
         status: res.status,
         msg: "Failed to fetch articles",
+      });
+    }
+
+    return res.json();
+  });
+}
+export function fetchArticleById(article_id) {
+  return fetch(
+    `https://nc-back-end.onrender.com/api/articles/${article_id}`,
+  ).then((res) => {
+    if (!res.ok) {
+      return Promise.reject({
+        status: res.status,
+        msg: "Failed to fetch article",
       });
     }
 
