e8aa80bd4a596b0d66b6c61ee342c593b7e4becd
J-Mro <j.k.mroueh@gmail.com>
2026-02-24 16:06:30 +0000

    add second route to view single article, remove unused Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import { Header } from "./components/Header";
-import { Nav } from "./components/Nav";
 import { ArticlesList } from "./components/ArticlesList";
+import { SingleArticle } from "./components/SingleArticle";
 import { Routes, Route } from "react-router";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Nav /> */}
       <Routes>
         <Route path="/" element={<ArticlesList />} />
+        <Route path="/articles/:article_id" element={<SingleArticle />} />
       </Routes>
     </>
   );
