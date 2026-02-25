ec438e48e6570a140d8e59dcd2260693eb2e6181
J-Mro <j.k.mroueh@gmail.com>
2026-02-25 11:22:11 +0000

    add list of comments to single article page

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,17 +1,22 @@
 import { useParams } from "react-router";
 import { fetchArticleById } from "../utils/fetchArticleData";
 import { useEffect, useState } from "react";
+import { fetchCommentData } from "../utils/fetchCommentData";
 
-export function SingleArticle({ articleData }) {
+export function SingleArticle() {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
+  const [comments, setComments] = useState({});
   useEffect(() => {
     async function getData() {
-      const response = await fetchArticleById(article_id);
-      setArticle(response);
+      const responseArticle = await fetchArticleById(article_id);
+      const responseComments = await fetchCommentData(article_id);
+      setArticle(responseArticle);
+      setComments(responseComments);
     }
     getData();
   }, []);
+  console.log(comments);
   return (
     <section className="single-article-view">
       <h2>{article.title}</h2>
@@ -21,6 +26,20 @@ export function SingleArticle({ articleData }) {
       <img src={`${article.article_img_url}`} alt="" />
       <p>{article.body}</p>
       <p>votes: {article.votes}</p>
+      <section>
+        <h3>comments</h3>
+        {comments.comments &&
+          comments.comments.map((comment, index) => {
+            return (
+              <div>
+                <p>{new Date(comment.created_at).toLocaleString()}</p>
+                <p>{comment.body}</p>
+                <p>by: {comment.author}</p>
+                <p>votes: {comment.votes}</p>
+              </div>
+            );
+          })}
+      </section>
     </section>
   );
 }
