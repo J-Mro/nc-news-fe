7d13d004d1f97a65c2d5eeb10bc4793aea0a3a89
J-Mro <j.k.mroueh@gmail.com>
2026-02-26 21:20:39 +0000

    update ArticleLikeBtn component to differentate like and dislike disable states

--- src/components/ArticleLikeBtn.jsx
+++ src/components/ArticleLikeBtn.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { patchArticleVotes } from "../utils/patchArticleData";
 export function ArticleLikeBtn({ setVoteChangeState, article_id, article }) {
-  const [isDisabled, setIsDisabled] = useState(false);
+  const [isLikeDisabled, setIsLikeDisabled] = useState(false);
+  const [isDislikeDisabled, setIsDislikeDisabled] = useState(false);
   async function eventHandlerLike() {
     async function clickVoteBtn() {
       try {
-        if (isDisabled) {
+        if (isLikeDisabled) {
           return;
         }
         setVoteChangeState(1);
-        setIsDisabled(true);
+        setIsLikeDisabled(true);
         await patchArticleVotes(article_id, { inc_votes: 1 });
       } catch {
         setVoteChangeState(0);
@@ -20,11 +21,11 @@ export function ArticleLikeBtn({ setVoteChangeState, article_id, article }) {
   async function eventHandlerDislike() {
     async function clickVoteBtn() {
       try {
-        if (isDisabled) {
+        if (isDislikeDisabled) {
           return;
         }
         setVoteChangeState(-1);
-        setIsDisabled(true);
+        setIsDislikeDisabled(true);
         await patchArticleVotes(article_id, { inc_votes: -1 });
       } catch {
         setVoteChangeState(0);
@@ -34,10 +35,10 @@ export function ArticleLikeBtn({ setVoteChangeState, article_id, article }) {
   }
   return (
     <>
-      <button onClick={eventHandlerLike} disabled={isDisabled}>
+      <button onClick={eventHandlerLike} disabled={isLikeDisabled}>
         Like
       </button>
-      <button onClick={eventHandlerDislike} disabled={isDisabled}>
+      <button onClick={eventHandlerDislike} disabled={isDislikeDisabled}>
         Dislike
       </button>
     </>
