bf0987cc9d1d34d411940c4802d5a9ab32a95931
J-Mro <j.k.mroueh@gmail.com>
2026-02-26 21:45:36 +0000

    add Nav component to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import { SingleArticle } from "./components/SingleArticle";
 import { Routes, Route } from "react-router";
 import { UserProvider } from "./contexts/User";
 import { UsersList } from "./components/UsersList";
+import { Nav } from "./components/Nav";
 
 function App() {
   return (
     <UserProvider>
       <Header />
+      <Nav />
       <Routes>
         <Route path="/" element={<ArticlesList />} />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
