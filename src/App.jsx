17c784c36ddeff24ab4a43b5464094765d10cbd7
J-Mro <j.k.mroueh@gmail.com>
2026-02-26 14:57:51 +0000

    add UserProvider component to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import { Header } from "./components/Header";
 import { ArticlesList } from "./components/ArticlesList";
 import { SingleArticle } from "./components/SingleArticle";
 import { Routes, Route } from "react-router";
+import { UserProvider } from "./contexts/User";
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <Header />
       <Routes>
         <Route path="/" element={<ArticlesList />} />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
       </Routes>
-    </>
+    </UserProvider>
   );
 }
 
