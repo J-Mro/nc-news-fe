23b7816032cbe4bc7b38290f4bf06fa5c84b5770
J-Mro <j.k.mroueh@gmail.com>
2026-02-26 21:45:09 +0000

    add Nav component with Home and Users links for / and /users paths

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router";
+
+export function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
