e020d6c4a0ca13ada230b90fbea2762cacf2ccb5
J-Mro <j.k.mroueh@gmail.com>
2026-02-27 12:09:48 +0000

    add route for /topics to render TopicsList view

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router";
 import { UserProvider } from "./contexts/User";
 import { UsersList } from "./components/UsersList";
 import { Nav } from "./components/Nav";
+import { TopicsList } from "./components/TopicsList";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<ArticlesList />} />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
-        <Route path="/users" element={<UsersList />}></Route>
+        <Route path="/users" element={<UsersList />} />
+        <Route path="/topics" element={<TopicsList />} />
       </Routes>
     </UserProvider>
   );
