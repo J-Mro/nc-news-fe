52d2490502f3b9ffd7d0bf8a3afca49dcac23f4c
J-Mro <j.k.mroueh@gmail.com>
2026-02-27 11:22:24 +0000

    add conditionally rendered delete button for users comments

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { fetchCommentData } from "../utils/fetchCommentData";
 import { CommentForm } from "./CommentForm";
+import { UserContext } from "../contexts/User";
 
 export function CommentsList({ article_id }) {
   const [comments, setComments] = useState({});
+  const { loggedInUser } = useContext(UserContext);
   useEffect(() => {
     async function getData() {
       const responseComments = await fetchCommentData(article_id);
@@ -25,6 +27,9 @@ export function CommentsList({ article_id }) {
               <p>{comment.body}</p>
               <p>by: {comment.author}</p>
               <p>votes: {comment.votes}</p>
+              {loggedInUser.username === comment.author && (
+                <button>Delete</button>
+              )}
             </div>
           );
         })}
