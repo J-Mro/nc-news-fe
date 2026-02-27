5e44dcdbec8b6d2a6dba6fc386a5bdba81398317
J-Mro <j.k.mroueh@gmail.com>
2026-02-27 20:49:08 +0000

    add URL search params for sort by drop down

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -2,19 +2,34 @@ import { Link } from "react-router";
 import { fetchArticleData } from "../utils/fetchArticleData";
 import { ArticleCard } from "./ArticleCard";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
 
 export function ArticlesList() {
   const [articles, setArticles] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams({
+    sort_by: "created_at",
+  });
+  const sorting = searchParams.get("sort_by");
   useEffect(() => {
     async function getData() {
-      const response = await fetchArticleData();
+      const response = await fetchArticleData(sorting);
       setArticles(response);
     }
     getData();
-  }, []);
+  }, [sorting]);
+  function changeHandlerSorting(e) {
+    setSearchParams({ sort_by: e.target.value });
+  }
+
   return (
     <section>
       <h2>Articles</h2>
+      <div className="article-list-query-btns">
+        <select name="sort-by" id="sort-by" onChange={changeHandlerSorting}>
+          <option value="created_at">Date</option>
+          <option value="votes">Likes</option>
+        </select>
+      </div>
       {articles.length !== 0 &&
         articles.map((article, index) => {
           return (
