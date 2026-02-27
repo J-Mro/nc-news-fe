a259d2899dceba670206708e520a0048d554e053
J-Mro <j.k.mroueh@gmail.com>
2026-02-27 11:51:05 +0000

    add deleteComment util function to delete button event handler

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,15 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../contexts/User";
+import { deleteComment } from "../utils/deleteComment";
 
 export function CommentCard({ comment }) {
+  const [isDeleted, setIsDeleted] = useState(false);
   const { loggedInUser } = useContext(UserContext);
-  return (
-    <div>
-      <p>{new Date(comment.created_at).toLocaleString()}</p>
-      <p>{comment.body}</p>
-      <p>by: {comment.author}</p>
-      <p>votes: {comment.votes}</p>
-      {loggedInUser.username === comment.author && <button>Delete</button>}
-    </div>
-  );
+
+  function handleClick() {
+    async function removePost() {
+      await deleteComment(comment.comment_id);
+      setIsDeleted(true);
+    }
+    removePost();
+  }
+  if (!isDeleted) {
+    return (
+      <div>
+        <p>{new Date(comment.created_at).toLocaleString()}</p>
+        <p>{comment.body}</p>
+        <p>by: {comment.author}</p>
+        <p>votes: {comment.votes}</p>
+        {loggedInUser.username === comment.author && (
+          <button onClick={handleClick}>Delete</button>
+        )}
+      </div>
+    );
+  } else {
+    return <div>You've deleted this post. Goodbye, post! 👋</div>;
+  }
 }
