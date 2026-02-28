915e44a643e9d1f1691b12250a6fa0a3214e0d4f
J-Mro <j.k.mroueh@gmail.com>
2026-02-28 08:01:30 +0000

    add ArticleNotFoundError component to extract error logic from SingleArticleComponent

---
+++ src/components/ArticleNotFoundError.jsx
@@ -0,0 +1,7 @@
+export function ArticleNotFoundError() {
+  return (
+    <section className="article-does-not-exist-error">
+      <h2>Sorry, the article you're looking for does not exist</h2>
+    </section>
+  );
+}
