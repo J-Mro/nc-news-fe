1785e1ec52fb0dfa31f2262a676a0b4653655093
J-Mro <j.k.mroueh@gmail.com>
2026-02-28 20:48:14 +0000

    add error handling to SingleTopicList component for not found errors

--- src/components/SingleTopicList.jsx
+++ src/components/SingleTopicList.jsx
@@ -6,28 +6,42 @@ import { Link } from "react-router";
 
 export function SingleTopicList() {
   const [filteredArticles, setFilteredArticles] = useState([]);
+  const [errStatus, setErrStatus] = useState(200);
   const { topic } = useParams();
   useEffect(() => {
     async function getData() {
-      const res = await fetchArticlesByTopic(topic);
-      setFilteredArticles(res);
+      try {
+        const res = await fetchArticlesByTopic(topic);
+        setFilteredArticles(res);
+      } catch (err) {
+        setErrStatus(err.status);
+      }
     }
     getData();
   }, []);
-  return (
-    <section>
-      <h2>Topic: {topic}</h2>
-      {filteredArticles.length !== 0 &&
-        filteredArticles.map((article, index) => {
-          return (
-            <Link to={`/articles/${article.article_id}`}>
-              <ArticleCard
-                key={`${index}+${article.title}`}
-                articleData={article}
-              />
-            </Link>
-          );
-        })}
-    </section>
-  );
+  if (errStatus !== 200) {
+    return (
+      <>
+        <h2>404</h2>
+        <h3>Uh oh! The topic you're looking for does not exist</h3>
+      </>
+    );
+  } else {
+    return (
+      <section>
+        <h2>Topic: {topic}</h2>
+        {filteredArticles.length !== 0 &&
+          filteredArticles.map((article, index) => {
+            return (
+              <Link to={`/articles/${article.article_id}`}>
+                <ArticleCard
+                  key={`${index}+${article.title}`}
+                  articleData={article}
+                />
+              </Link>
+            );
+          })}
+      </section>
+    );
+  }
 }
