10ef0b431e0a78e2a5fa274b52c20610b5a420f3
J-Mro <j.k.mroueh@gmail.com>
2026-03-01 17:01:58 +0000

    update UsersList component to utilise useLoadingError hook

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,25 +1,25 @@
 import { fetchUserData } from "../utils/fetchUserData";
-import { useEffect, useState } from "react";
 import { UserCard } from "./UserCard";
+import { useLoadingError } from "../hooks/useLoadingError";
 export function UsersList() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      const res = await fetchUserData();
-      setUsers(res);
-    }
-    getData();
-  }, []);
-
-  return (
-    <section>
-      <h2>Users</h2>
-      <h3>Who's browsing?</h3>
-      {users.length !== 0 &&
-        users.map((user, index) => {
-          return <UserCard user={user} />;
-        })}
-    </section>
-  );
+  const [data, setData, isLoading, error] = useLoadingError(fetchUserData, {});
+  const users = data;
+  if (isLoading || error) {
+    return (
+      <section>
+        {isLoading ? <p>Loading...</p> : <h3>Could not fetch users 😔</h3>}
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <h2>Users</h2>
+        <h3>Who's browsing?</h3>
+        {users.length !== 0 &&
+          users.map((user, index) => {
+            return <UserCard user={user} />;
+          })}
+      </section>
+    );
+  }
 }
