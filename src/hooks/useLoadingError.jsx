e9dfeb74f112886212b0cb050057480f1b45917d
J-Mro <j.k.mroueh@gmail.com>
2026-03-01 15:20:44 +0000

    add useLoadingError hook

---
+++ src/hooks/useLoadingError.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+export function useLoadingError(reqFunction, options) {
+  const { dependencies = [], params = [] } = options;
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    async function setUp() {
+      setIsLoading(true);
+      try {
+        const newData = await reqFunction(...params);
+        setData(newData);
+        setError(null);
+      } catch (err) {
+        setError(err);
+        setData(null);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    setUp();
+  }, [...dependencies]);
+  return [data, isLoading, error];
+}
