a768ddd2d05d66209108d4862eb7a88b203e0824
J-Mro <j.k.mroueh@gmail.com>
2026-03-04 20:31:05 +0000

    update Nav component to include class for styling and simple graphics

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,18 +2,16 @@ import { Link } from "react-router";
 
 export function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
+    <nav className="nav-buttons">
+      <Link to="/">
+        <h2>🏠 Home</h2>
+      </Link>
+      <Link to="/users">
+        <h2>👤 Users</h2>
+      </Link>
+      <Link to="/topics">
+        <h2>💡 Topics</h2>
+      </Link>
     </nav>
   );
 }
