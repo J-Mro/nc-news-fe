5598b6d9bb271f58fe92baec241cb02da162d390
J-Mro <j.k.mroueh@gmail.com>
2026-03-04 20:29:00 +0000

    update header with web app name change and additional styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import { LoginInfo } from "./LoginInfo";
 import { Link } from "react-router";
 export function Header() {
   return (
-    <header>
+    <header className="header-component">
       <Link to="/">
-        <h1 id="nc-news-header-text">NC News</h1>
+        <h1 className="logo" id="nc-news-header-text">
+          NorthStar<span className="logo-star">*</span>
+        </h1>
       </Link>
       <LoginInfo />
     </header>
