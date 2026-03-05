677a5dbce99bf9811b1716500065478baf423c1a
J-Mro <j.k.mroueh@gmail.com>
2026-03-05 19:24:16 +0000

    add className to div tag for styling and image to replace null for img src prop

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,7 +1,15 @@
 export function TopicCard({ topic }) {
   return (
-    <div>
-      <img src={topic.img_url === "" ? null : `${topic.img_url}`} alt="" />
+    <div className="topic-card">
+      <img
+        src={
+          topic.img_url === ""
+            ? "images/computer-program-coding-screen_53876-138060.png"
+            : `${topic.img_url}`
+        }
+        alt={`image of ${topic.slug}`}
+        width={600}
+      />
       <h3>{topic.slug}</h3>
       <p>{topic.description}</p>
     </div>
